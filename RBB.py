dfd202153b33b56e81dd90b2c6522ccf4eb41fb0
stcline <steve.cline@weldre4.org>
2023-03-06 15:33:05 -0700

    Add RBB.py script - final script

---
+++ RBB.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This script runs a Raspberry Pi Zero W in order to record flight data.
+
+The equipment included in this device are:
+  - Raspberry Pi Zero W
+  - HD Camera Module with wide angle lens
+  - GY-521 3 Axis Accelerometer Gyroscope Module
+  - piezoelelctric buzzer
+  - LED of any color
+
+The script does the following:
+
+"""
+
+# import modules
+
+
+
