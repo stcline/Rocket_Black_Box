1270894a2438369ae5153e16c6b0ede8a9ed3a6c
Steve Cline <steve.cline@weldre4.org>
2023-03-06 20:26:35 -0700

    Add Button_start.py

---
+++ Python/Button_start.py
@@ -0,0 +1,17 @@
+# Attach a button to GPIO 23 and set it up as an input
+# When the button is pressed, another script is run
+# The script is run as a background process so the button can be pressed again
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(23)
+    if input_state == False:
+        print('Button Pressed')
+        subprocess.Popen(['python', 'Button_stop.py'])
+        time.sleep(0.2)
