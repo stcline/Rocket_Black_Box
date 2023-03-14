871e82fbea92e46f0eb3569fb9c92d21c5f81542
stcline <steve.cline@weldre4.org>
2023-03-13 19:30:47 -0600

    Add video file to repo

--- Python/camera/rec_5_sec.py
+++ Python/camera/rec_5_sec.py
@@ -4,7 +4,7 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-camera.start_recording('/home/pi/Rocket_Lab/Rocket_Black_Box/video.h264')
+camera.start_recording('/home/pi/Documents/Rocket_Black_Box/video.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
