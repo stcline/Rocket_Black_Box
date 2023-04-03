8a69bfd52f6ef3ff229b2ec973efa7af4e213636
Steve Cline <steve.cline@weldre4.org>
2023-04-02 19:30:48 -0600

    modify MPU6050_CSV.py

--- Python/MPU/MPU6050_CSV.py
+++ Python/MPU/MPU6050_CSV.py
@@ -16,32 +16,32 @@ power_mgmt_2 = 0x6c
 
 # Read register data
 def read_byte(reg):
-    return bus.read_byte_data(address, reg)
+	return bus.read_byte_data(address, reg)
 
 def read_word(reg):
-    h = bus.read_byte_data(address, reg)
-    l = bus.read_byte_data(address, reg +1)
-    value = (h << 8) + l
-    return value
+	h = bus.read_byte_data(address, reg)
+	l = bus.read_byte_data(address, reg +1)
+	value = (h << 8) + l
+	return value
 
 def read_word_2c(reg):
-    val = read_word(reg)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
+	val = read_word(reg)
+	if (val >= 0x8000):
+		return -((65535 - val) + 1)
+	else:
+		return val
 
 # Convert raw register data to x,y and accelleration
 def dist(a ,b):
-    return math.sqrt(( a *a ) +( b *b))
+	return math.sqrt(( a *a ) +( b *b))
 
 def get_y_rotation(x ,y ,z):
-    radians = math.atan2(x, dist(y ,z))
-    return -math.degrees(radians)
+	radians = math.atan2(x, dist(y ,z))
+	return -math.degrees(radians)
 
 def get_x_rotation(x ,y ,z):
-    radians = math.atan2(y, dist(x ,z))
-    return math.degrees(radians)
+	radians = math.atan2(y, dist(x ,z))
+	return math.degrees(radians)
 
 bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
 address = 0x68       # via i2cdetect
@@ -74,7 +74,7 @@ while True:
 		logwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 		logwriter.writerow([now.strftime("%Y")] + [now.strftime("%d")] + [now.strftime("%m")] + [now.strftime("%H")] + [now.strftime("%M")] + [now.strftime("%S.%f")] + [Gx] + [Gy] + [Gz] + [Ax] +[Ay] + [Az] + [Ax_scaled] + [Ay_scaled] + [Az_scaled])
   
-    time.sleep(.01)
+	time.sleep(.01)
 
-    if time.time() > timeout:
-        break
+	if time.time() > timeout:
+		break
